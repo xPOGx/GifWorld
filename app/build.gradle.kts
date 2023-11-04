feb275f245110b9ba13aca4bec274242d58ecd51
POG <pavel19.1078@gmail.com>
2023-11-04 16:31:00 +0200

    New Resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
 }
 
 android {
@@ -40,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -50,20 +51,43 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    // COMPOSE
+    implementation(platform("androidx.compose:compose-bom:${rootProject.extra["bom_version"]}"))
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // LIFECYCLE
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
+
+    // DI
+    // Dagger
+    implementation("com.google.dagger:dagger:${rootProject.extra["dagger_version"]}")
+    ksp("com.google.dagger:dagger-compiler:${rootProject.extra["dagger_version"]}")
+
+    // NAVIGATION
+    implementation("androidx.navigation:navigation-compose:${rootProject.extra["nav_version"]}")
+
+    // NETWORK
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    // Moshi/converter
+    implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:${rootProject.extra["coil_version"]}")
+    implementation("io.coil-kt:coil-gif:${rootProject.extra["coil_version"]}")
+
+    // DATABASE
+    // Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("nav_version", "2.6.0")
+        set("room_version", "2.5.2")
+        set("lifecycle_version", "2.6.2")
+        set("bom_version", "2023.06.01")
+        set("dagger_version", "2.48")
+        set("coil_version", "2.4.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
